799265d15cd5766b6034b731845f9532d2a306d2
dimitargr <dimitar.gr.dimitrov@gmail.com>
2024-05-17 15:47:44 +0300

    Update streamlit_app.py
    Added Fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 name_on_order = st.text_input('What is your name', '')
 st.write('Your name is ', name_on_order)
@@ -15,6 +16,9 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients', 
